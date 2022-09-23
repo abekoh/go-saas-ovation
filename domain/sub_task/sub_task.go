405762a2d5b8fd279f2915b591b7d76265f89d41
Kotaro Abe <abekoh.bcky@gmail.com>
2022-09-23 12:42:46 +0900

    feat: subtask

---
+++ domain/sub_task/sub_task.go
@@ -0,0 +1,21 @@
+package subtask
+
+import (
+	"context"
+
+	"github.com/abekoh/go-saas-ovation/domain/backlog_item"
+	"github.com/google/uuid"
+)
+
+type ID uuid.UUID
+
+type SubTask struct {
+	ID            ID             `json:"id"`
+	Summary       string         `json:"summary"`
+	StoryPoint    *int           `json:"storyPoint"`
+	BackLogItemID backlogitem.ID `json:"backLogItemId"`
+}
+
+type Repository interface {
+	GetOne(ctx context.Context, id ID) (*SubTask, error)
+}
